9c9e60c211dc4d38de25fdc19aac4b6d0488773d
David Nichol <david_r_nichol@yahoo.com>
2014-10-20 21:35:12 -0400

    replaced underscore with handlebars templating

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 var http = require('http');
 var koa = require('koa');
 var views = require('koa-views');
+var handlebars = require('koa-handlebars');
 var routing = require('koa-routing');
 var app = koa();
 
-app.use(views('views', {
-	cache: true,
-	map: {
-		html: 'underscore'
-	}
+app.use(handlebars({
+	defaultLayout: "main"
 }));
+
 app.use(routing(app));
 
 app.route('/signup')
 	.get(function * (next) {
-		yield this.render('index.html', {
+		yield this.render('index', {
 			name: "Parashat Eikev",
-			torahReading: "Deuteronomy 7.12-11.25",
-			haftarahReading: "Isaiah 49.14-51.3"
+			duties: [{
+				description: "Torah",
+				details: "Deuteronomy 7.12-11.25",
+				volunteer: "Sammy Chateau"
+			},
+			{
+				description: "Haftarah",
+				details: "Isaiah 49.14-51.3"
+			}]
 		});
 
 		yield next;
