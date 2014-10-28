6527688a0e4366dbeb66a1ebf9013fff15a98062
David Nichol <david_r_nichol@yahoo.com>
2014-10-27 22:04:58 -0400

    Facebook auth

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ var koa = require('koa');
 var views = require('koa-views');
 var handlebars = require('koa-handlebars');
 var routing = require('koa-routing');
+var passport = require("koa-passport");
+var FacebookStrategy = require("passport-facebook").Strategy;
 var app = koa();
 
 app.use(handlebars({
 	defaultLayout: "main"
 }));
 
+passport.use(new FacebookStrategy({
+	clientID: process.env.FACEBOOK_APPID,
+	clientSecret: process.env.FACEBOOK_SECRET,
+	callbackURL: "http://www.dnix.us"
+},
+	function(accessToken, refreshToken, profile, done) {
+		console.log("Received token for " + profile.name.familyName);
+	}
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(routing(app));
 
 app.route('/')
@@ -16,6 +30,12 @@ app.route('/')
 		yield this.render('welcome', {});
 		yield next;
 	});
+
+app.route('/auth/facebook').get(passport.authenticate('facebook'));
+
+app.route('/auth/facebook/callback').get(
+	passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/loginFailure'})
+);
 
 app.route('/signup')
 	.get(function * (next) {
